2845520ec733f9f0a0ccba21b700cc3c60fde83f
Eliezel7 <promiseseliezel@gmail.com>
2023-10-13 14:12:58 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *  mu1 - a function that multiplies two integers
+ * :mul - a function that multiplies two integers
  * @a: The first number to multiply
  * @b: The second number to multiply with first number
- * Return: The result of multiplication between a param and b param	
+ * Return: The result of multiplication between a param and b param
  */
-int mu1(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
